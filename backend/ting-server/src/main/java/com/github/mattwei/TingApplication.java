5ec55b9b580b2c99b6aeec4aef739f38a0461f4a
a85009456 <a85009546@gmail.com>
2025-01-10 22:50:36 +0800

    init: 初始化後端環境

---
+++ backend/ting-server/src/main/java/com/github/mattwei/TingApplication.java
@@ -0,0 +1,15 @@
+package com.github.mattwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class TingApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(TingApplication.class, args);
+    }
+}
