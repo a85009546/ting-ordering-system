310b8a522ad4673db3cee82126c468e6a28b5e71
a85009456 <a85009546@gmail.com>
2025-01-15 23:01:09 +0800

    init: 初始化前端環境

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import LayoutView from '@/views/layout/index.vue'
+</script>
+
+<template>
+  <LayoutView></LayoutView>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/admin/category/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  分類管理
+</template>
+
+<style scoped>
+
+</style>
