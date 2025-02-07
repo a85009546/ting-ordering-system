eee7b5dd0a789958c8cb46774b3546a035f6c840
a85009456 <a85009546@gmail.com>
2025-02-07 11:15:03 +0800

    feat: 將配置文件脫敏

--- backend/ting-server/src/main/java/com/github/mattwei/TingApplication.java
+++ backend/ting-server/src/main/java/com/github/mattwei/TingApplication.java
@@ -1,5 +1,6 @@
 package com.github.mattwei;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement // 開啟註解方式的事務管理
 @EnableScheduling // 開啟任務調度
+@EnableEncryptableProperties // 啟用 Jasypt 屬性加密
 @Slf4j
 public class TingApplication {
     public static void main( String[] args ) {
